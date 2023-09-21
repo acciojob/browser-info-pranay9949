ed71142f5849f0171c64fb7f4051c0fd48ae1e4c
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-21 12:33:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 let a=document.getElementById("browser-info");
 let b=document.createElement("p")
-b.append("You are using "+  navigator.appName+" version "+ navigator.userAgent+  navigator.appVersion);
+b.innerText="You are using "+  navigator.appName+" version "+ navigator.userAgent+  navigator.appVersion
 a.append(b);
