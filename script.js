85d5326cb5ab90daca44ac5edab04fa0062845a9
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-21 12:02:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 let a=document.getElementById("browser-info");
 let b=document.createElement("p")
-b.append(`You are using ${navigator.userAgent} ${navigator.appName} ${navigator.appVersion}`);
+b.append(`You are using ${navigator.userAgent} version  ${navigator.appName} ${navigator.appVersion}`);
 a.append(b);
